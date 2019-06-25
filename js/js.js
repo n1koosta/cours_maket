a3ebef58b27f7c7d7ba56cf63fd8f9cca29df92c
n1koosta <52212927+n1koosta@users.noreply.github.com>
2019-06-25 21:30:58 +0300

    new

---
+++ js/js.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function (event) {
+
+
+var arr_h2=document.querySelectorAll('#ul1');
+for (var i=0;i<arr.length;i++){
+    arr_h2[i].style.fontFamily='Arial';
+    arr_h2[i].style.color='red';
+    arr_h2[i].style.fontSize='50px';
+
+}
+arr_h2[1].style.color='Yellow';
+arr_h2[2].style.color='green';
+
+ });
